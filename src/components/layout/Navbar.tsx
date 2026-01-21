ae59673c6434a2bb37fbc080d0d3453a279cbe2d
Harris-PS <psharris2002@gmail.com>
2026-01-21 14:44:21 +0530

    inital commit

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,18 @@
+function Navbar(){
+  return(
+    
+    <nav className="navbar">
+      <div className="logo">
+        <img src="" alt="" />
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>Login</li>
+        <li>Sign Up</li> 
+      </ul>
+    </nav>
+    
+  )
+}
+
+export default Navbar
